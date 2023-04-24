2007ceb5294a0352cacd109276f0844832019094
RohitKV1n0d <rohitvinod92@gmail.com>
2023-04-24 23:47:00 +0530

    db edit

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from testPredict import predict # predict('Purchase', 100, 12, 'Chennai')
 app = Flask(__name__)
 
 
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev' :
     app.debug = True
--- createdb.py
+++ createdb.py
@@ -1,5 +1,23 @@
-from app import app,db
+from app import app,db, User
 with app.app_context():
     db.create_all()
+
+
+# class User(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     f_name = db.Column(db.String(200), nullable=False)
+#     l_name = db.Column(db.String(20), nullable=False)
+#     email = db.Column(db.String(100), nullable=False, unique=True)
+#     username = db.Column(db.String(200))
+#     pwd = db.Column(db.String(100), nullable=False)
+#     # phone = db.Column(db.Integer, nullable=False)
+#     balance = db.Column(db.Integer, nullable=False)
+#     transactions = db.relationship('Transaction_history', backref='trans')
+#     creditcard = db.relationship('Card_details', backref='credit')
+# create admin user
+admin = User(username='anjana',f_name='Anjana',l_name='Paul',email='anjanapaul0614@gmail.com',pwd='1234',balance=100000)
+db.session.add(admin)
+db.session.commit()
+
 
 print("database Created")
